a02060fce83dafe1a9c25c489a91a4d4082df170
Xavier S <xavier.slattery@gmail.com>
2018-03-11 15:56:37 +1100

    Rework of the cocoawindowing backend

--- src/demo/demomain.cpp
+++ src/demo/demomain.cpp
@@ -7,34 +7,30 @@
 #define DEBUG 1
 
 #if DEBUG
-#define GLCALL(x) x; check_opengl_errors( __LINE__ );
+#define GLCALL check_opengl_errors(__LINE__);
 #else
-#define GLCALL(x) x;
+#define GLCALL ;
 #endif
 
-static void check_opengl_errors ( size_t line )
-{
-	while ( GLenum error = glGetError() )
-	{
+static inline void check_opengl_errors ( size_t line ) {
+	while (GLenum error = glGetError()) {
 		std::cout << "OpenGL Error: L:" << line << " : " << error << '\n';
 	}
 }
 
-static unsigned int compile_shader ( unsigned int type, const std::string& source )
-{
-	unsigned int shader = GLCALL( glCreateShader( type ) );
+static unsigned int compile_shader ( unsigned int type, const std::string& source ) {
+	unsigned int shader = glCreateShader(type); GLCALL;
 	const char *src = source.c_str();
-	GLCALL( glShaderSource( shader, 1, &src, nullptr ) );
-	GLCALL( glCompileShader( shader ) );
+	glShaderSource(shader, 1, &src, nullptr); GLCALL;
+	glCompileShader(shader); GLCALL;
 
 	int result;
-	GLCALL( glGetShaderiv( shader, GL_COMPILE_STATUS, &result ) );
-	if ( result == GL_FALSE )
-	{
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &result); GLCALL;
+	if (result == GL_FALSE) {
 		int length;
-		GLCALL( glGetShaderiv( shader, GL_INFO_LOG_LENGTH, &length ) );
-		char* message = (char*)alloca( length * sizeof(char) );
-		GLCALL( glGetShaderInfoLog( shader, length, &length, message ) );
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length); GLCALL;
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(shader, length, &length, message); GLCALL;
 		std::cout << "Shader Failed to Compile: " << '\n';
 		std::cout << message << '\n';
 	}
@@ -42,44 +38,40 @@ static unsigned int compile_shader ( unsigned int type, const std::string& sourc
 	return shader;
 }
 
-static unsigned int load_shader ( const std::string& vertexShader, const std::string& fragementShader )
-{
-	unsigned int program = GLCALL( glCreateProgram() );
-	unsigned int vs = compile_shader( GL_VERTEX_SHADER, vertexShader );
-	unsigned int fs = compile_shader( GL_FRAGMENT_SHADER, fragementShader );
+static unsigned int load_shader ( const std::string& vertexShader, const std::string& fragementShader ) {
+	unsigned int program = glCreateProgram(); GLCALL;
+	unsigned int vs = compile_shader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = compile_shader(GL_FRAGMENT_SHADER, fragementShader);
 
-	GLCALL( glAttachShader( program, vs) );
-	GLCALL( glAttachShader( program, fs) );
-	GLCALL( glLinkProgram( program ) );
-	GLCALL( glValidateProgram( program ) );
+	glAttachShader(program, vs); GLCALL;
+	glAttachShader(program, fs); GLCALL;
+	glLinkProgram(program); GLCALL;
+	glValidateProgram(program); GLCALL;
 
-	GLCALL( glDetachShader( program, vs ) );
-	GLCALL( glDetachShader( program, fs ) );
+	glDetachShader(program, vs); GLCALL;
+	glDetachShader(program, fs); GLCALL;
 
-	GLCALL( glDeleteShader(vs) );
-	GLCALL( glDeleteShader(fs) );
+	glDeleteShader(vs); GLCALL;
+	glDeleteShader(fs); GLCALL;
 
 	return program;
 }
 
-int main ( int argc, char const *argv[] )
-{
-	init_application();
-	create_window( "Cocoa Window", 640, 400 );
-
-	set_window_transparency( true );
+int main ( int argc, char const **argv ) {
+	app_init();
+	window_create("Cocoa Window", 640, 400);
 	
 	std::cout << "OpenGL Vendor: " << glGetString(GL_VENDOR) << '\n';
 	std::cout << "OpenGL Renderer: " << glGetString(GL_RENDERER) << '\n';
 	std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << '\n';
 	std::cout << "OpenGL Shading Language: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << '\n';
 
-	GLCALL( glEnable( GL_FRAMEBUFFER_SRGB ) );
-	GLCALL( glEnable( GL_DEPTH_TEST ) );
-	GLCALL( glEnable( GL_CULL_FACE ) );
-	GLCALL( glCullFace( GL_BACK ) );
+	glEnable(GL_FRAMEBUFFER_SRGB); GLCALL;
+	glEnable(GL_DEPTH_TEST); GLCALL;
+	glEnable(GL_CULL_FACE); GLCALL;
+	glCullFace(GL_BACK); GLCALL;
 
-	GLCALL( glClearColor( 0.1f, 0.1f, 0.15f, 0.8f ) );
+	glClearColor(0.1f, 0.1f, 0.15f, 1.0f); GLCALL;
 
 	unsigned int shader = load_shader(
 		R"(
@@ -110,48 +102,63 @@ int main ( int argc, char const *argv[] )
 		)"
 	);
 
-	GLCALL( glUseProgram( shader ) );
+	glUseProgram(shader); GLCALL;
 
-	float verts [] =
-	{
+	float verts [] = {
 		-0.5f, -0.5f, 	1.0f, 0.0f, 0.0f, 1.0f,
 		0.5f, -0.5f, 	0.0f, 1.0f, 0.0f, 1.0f,
 		0.0f, 0.5f, 	0.0f, 0.0f, 1.0f, 1.0f
 	};
 
 	unsigned int vao;
-	GLCALL( glGenVertexArrays( 1, &vao ) );
-	GLCALL( glBindVertexArray( vao ) );
+	glGenVertexArrays(1, &vao); GLCALL;
+	glBindVertexArray(vao); GLCALL;
 
 	unsigned int vbo;
-	GLCALL( glGenBuffers( 1, &vbo ) );
-	GLCALL( glBindBuffer( GL_ARRAY_BUFFER, vbo ) );
-	GLCALL( glBufferData( GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW ) );
+	glGenBuffers(1, &vbo); GLCALL;
+	glBindBuffer(GL_ARRAY_BUFFER, vbo); GLCALL;
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW); GLCALL;
 
-	GLCALL( glEnableVertexAttribArray( 0 ) );
-	GLCALL( glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*6, 0 ) );
+	glEnableVertexAttribArray(0); GLCALL;
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*6, 0); GLCALL;
 
-	GLCALL( glEnableVertexAttribArray( 1 ) );
-	GLCALL( glVertexAttribPointer( 1, 4, GL_FLOAT, GL_FALSE, sizeof(float)*6, (void*)(sizeof(float)*2) ) );
+	glEnableVertexAttribArray(1); GLCALL;
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float)*6, (void *)(sizeof(float)*2)); GLCALL;
 
-	while ( !get_window_is_closing() )
-	{
-		process_window_events();
+	while (!window_get_is_closed()) {
+		window_process_events();
 
-		if ( get_key_down(Keys::KEY_F) ) set_window_fullscreen( true );
-		if ( get_key_down(Keys::KEY_G) ) set_window_fullscreen( false );
-		if ( get_key_down(Keys::KEY_R) ) set_window_size( 1280, 720 );
-		if ( get_key_down(Keys::KEY_E) ) set_window_size( 640, 480 );
+		if (input_get_key_down(Key::K_SPACE)) {
+			window_set_complete_fullscreen(!window_get_complete_fullscreen());
+		}
 
-		GLCALL( glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT ) );
+		if (input_get_key_down(Key::K_F)) {
+			window_set_fullscreen(!window_get_fullscreen());
+		}
+
+		if (input_get_key_down(Key::K_1)) {
+			window_set_size(640, 480);
+		}
 		
-		GLCALL( glDrawArrays( GL_TRIANGLES, 0, 3 ) );
+		if (input_get_key_down(Key::K_2)) {
+			window_set_size(1280, 720);
+		}
 
-		refresh_window();
+		if (input_get_key_down(Key::K_C)) {
+			window_close();
+		}
+
+		if (glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE) {
+			glViewport(0, 0, window_get_width_hidpi(), window_get_height_hidpi()); GLCALL;
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); GLCALL;
+			
+			glDrawArrays(GL_TRIANGLES, 0, 3); GLCALL;
+		}
+
+		window_draw();
 	}
 
-	close_window();
-	close_application();
+	app_quit();
 
 	return 0;
 }
